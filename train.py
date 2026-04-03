7e3b9947d579aeef7c276905c8dabdf76cb69f50
Gabriella Pompeu <gpdnunes@id.uff.br>
2026-04-03 17:46:53 -0300

    Update train.py

--- train.py
+++ train.py
@@ -59,47 +59,36 @@ H = {"train_loss": [], "test_loss": []}
 print("[INFO] training the network...")
 startTime = time.time()
 for e in tqdm(range(config.NUM_EPOCHS)):
-	# set the model in training mode
 	unet.train()
-	# initialize the total training and validation loss
 	totalTrainLoss = 0
 	totalTestLoss = 0
-	# loop over the training set
+
 	for (i, (x, y)) in enumerate(trainLoader):
-		# send the input to the device
 		(x, y) = (x.to(config.DEVICE), y.to(config.DEVICE))
-		# perform a forward pass and calculate the training loss
 		pred = unet(x)
 		loss = lossFunc(pred, y)
-		# first, zero out any previously accumulated gradients, then
-		# perform backpropagation, and then update model parameters
 		opt.zero_grad()
 		loss.backward()
 		opt.step()
-		# add the loss to the total training loss so far
 		totalTrainLoss += loss
-	# switch off autograd
+
 	with torch.no_grad():
-		# set the model in evaluation mode
 		unet.eval()
-		# loop over the validation set
 		for (x, y) in testLoader:
-			# send the input to the device
 			(x, y) = (x.to(config.DEVICE), y.to(config.DEVICE))
-			# make the predictions and calculate the validation loss
 			pred = unet(x)
 			totalTestLoss += lossFunc(pred, y)
-	# calculate the average training and validation loss
+
 	avgTrainLoss = totalTrainLoss / trainSteps
 	avgTestLoss = totalTestLoss / testSteps
-	# update our training history
+
 	H["train_loss"].append(avgTrainLoss.cpu().detach().numpy())
 	H["test_loss"].append(avgTestLoss.cpu().detach().numpy())
-	# print the model training and validation information
+
 	print("[INFO] EPOCH: {}/{}".format(e + 1, config.NUM_EPOCHS))
 	print("Train loss: {:.6f}, Test loss: {:.4f}".format(
 		avgTrainLoss, avgTestLoss))
-# display the total time needed to perform the training
+
 endTime = time.time()
 print("[INFO] total time taken to train the model: {:.2f}s".format(
 	endTime - startTime))
@@ -114,5 +103,4 @@ plt.xlabel("Epoch #")
 plt.ylabel("Loss")
 plt.legend(loc="lower left")
 plt.savefig(config.PLOT_PATH)
-# serialize the model to disk
 torch.save(unet, config.MODEL_PATH)
